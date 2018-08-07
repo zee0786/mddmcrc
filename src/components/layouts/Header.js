b45eb06604d825d370bb12ce12ae446fac0da560
zee0786 <zee0786@gmail.com>
2018-08-07 23:26:38 +0530

    Add files via upload
    initial commit

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core';
+import SearchBar from '../composite/SearchBar';
+
+export default class Header extends Component{
+    render(){
+        return (
+            <header className='row' style={{background:'#4A234F'}}>
+                <div className='col-sm-4 col-md-2 text-center'>
+                    <img style={{width:'100px'}} src="../../assets/img/logo.png" />
+                </div>
+                <div className='col-sm-6 col-md-3'>
+                    <h3 style={{color:'white'}}>MDD Medical College &amp; Research Center</h3>
+                </div>
+                <SearchBar />
+                
+               {/* <AppBar  /> */}
+            </header>
+        )
+    }
+}
